0dfb3e1251ec61cf4e8cebc47cf3456b6da7d783
Eyalnavon1 <152074117+Eyalnavon1@users.noreply.github.com>
2023-12-03 15:40:21 +0200

    Update Shiur3.java

--- Shiur3.java
+++ Shiur3.java
@@ -1,1 +1,9 @@
-;fln;k l;k nl;klk
+public static void main(String[] args)
+{
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter two number");
+        int number = myObj.nextInt();  // Read user input
+        int second_number=myObj.nextInt();
+        int result = (number > second_number) ? second_number : number;
+        System.out.println(result); 
+}
